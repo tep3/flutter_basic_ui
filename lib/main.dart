74fd670a8e5d3ea74479e21fbbf7a8177ac2fd6c
tanatep <tep3@hotmail.com>
2019-11-02 23:24:50 +0700

    version 1.0

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+main() {
+  runApp(QuoteApp());
+}
+
+class QuoteApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Quote',
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('quote'),
+        
+      ),
+      body: Container(child: Text('test ..')),
+    );
+  }
+}
